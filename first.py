b8a8fa8ebce076e37b911a7145fc35e803a03be3
Soban Mehboob <soban.mehboob@econceptions.mobi>
2026-04-27 16:26:09 +0500

    calculation of any two integer numbers,Enter 2 numbers and select a sign

---
+++ first.py
@@ -0,0 +1,32 @@
+# calculation of any two integer numbers
+
+num_1=input("Enter First Value: ")
+num_2=input("Enter Second Value: ")
+
+
+if num_1.isdigit() and num_2.isdigit():
+    print("Proceeding")
+    sign=input("Select a sign: +,-,*,/  ")
+    if sign == "+" :
+       print(f"result is {int(num_1)+int(num_2)}")
+    elif sign=="-":
+        print(f"result is {int(num_1)-int(num_2)}")
+    elif sign=="*":
+        print(f"result is {int(num_1)*int(num_2)}")
+    elif sign == "/":
+        if num_2 == "0":
+            print("Cannot divide by zero!")
+        else:
+            print(f"result is {int(num_1) / int(num_2)}")
+    else:
+        print(f"'{sign}' is not a valid operator.")
+
+
+elif not num_1.isdigit() and not num_2.isdigit():
+    print("both arent valid")
+
+elif not num_1.isdigit():
+    print(f"enter a valid number in first field. {num_1} is not a number")
+
+elif not num_2.isdigit():
+    print(f"enter a valid number in second field. {num_2} is not a number")
